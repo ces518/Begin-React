74fb56660d8870a4e9beb2f473b9727aaaaa7f18
ces518 <pupupee9@gmail.com>
2019-11-02 21:35:30 +0900

    19.useReducer 기초

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,42 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer (state, action) {
+    switch (action.type) {
+        case 'INCREMENT': 
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;    
+        default:
+            // return state;
+            throw new Error('해당 하는 ACTION이 없습니다.');
+    }
+}
 
 // 클릭 이벤트가 발생했을때 특정 함수가 호출되도록 한다.
 function Counter () {
-    const [number, setNumber] = useState(0); // number 라는 상태를 사용하고, number 상태의 값을 0으로 지정해준다.
+    // const [number, setNumber] = useState(0); // number 라는 상태를 사용하고, number 상태의 값을 0으로 지정해준다.
+
+    // dispatch를 사용하여 액션을 발생시킨다.
+    const [number, dispatch] = useReducer(reducer, 0); // 첫번째 인자가 reducer 함수, 2번째인자는 초기값
 
     // number 를 1만큼 증가시킨다.
     const onIncrease = () => {
         // setNumber(number + 1);
         // 함수형 업데이트 방식
-        setNumber(prevNumber => prevNumber + 1);
+        // setNumber(prevNumber => prevNumber + 1);
         // 10
+        dispatch({
+            type: 'INCREMENT',
+        });
     };
 
     // number 를 1만큼 감소시킨다.
     const onDecrease = () => {
         // setNumber(number - 1);
-        setNumber(prevNumber => prevNumber - 1);
+        // setNumber(prevNumber => prevNumber - 1);
+        dispatch({
+            type: 'DECREMENT',
+        });
     };
 
     return (
