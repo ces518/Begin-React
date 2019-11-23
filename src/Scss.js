007bba2b2ad5f59b5d2b788cac616429762da338
ces518 <pupupee9@gmail.com>
2019-11-23 19:32:11 +0900

    36. Sass

---
+++ src/Scss.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './components/Button';
+import './App.scss';
+
+function Scss () {
+    return (
+        <div className="App">
+            <div className="buttons">
+                <Button>BUTTON</Button>
+            </div>
+        </div>
+    );
+};
+
+export default Scss;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Counter from './Counter';
+import Scss from './Scss';
 
 // id가 root인것을 찾아 리액트 앱을 넣어준다.
 // ReactApp을 랜더링할때는 root 엘리먼트 (div) 안에 랜더링 하는것이다.
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Scss />, document.getElementById('root'));
 
 
 serviceWorker.unregister();
