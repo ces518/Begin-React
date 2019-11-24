79cec5fb12b8bfddaa388b27a2efcf90f42db146
ces518 <pupupee9@gmail.com>
2019-11-24 20:46:25 +0900

    37.Sass버튼 A Size Props 설정하기

--- src/Scss.js
+++ src/Scss.js
@@ -6,7 +6,9 @@ function Scss () {
     return (
         <div className="App">
             <div className="buttons">
+                <Button size="large">BUTTON</Button>
                 <Button>BUTTON</Button>
+                <Button size="small">BUTTON</Button>
             </div>
         </div>
     );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Button.scss';
 
-function Button ({ children }) {
+// 버튼 사이즈: large, medium, small
+// 다양한 클래스 명들을 조건부로 줘야할때 유용한 모듈인 classnames 라는 모듈이 있다.
+// npm install classnames
+// 조건부로 다양한 클래스명을 줘야할때 편리하다.
+// 문자열, 배열, 객체 등 다양한 조건으로 클래스명을 조합할 수 있다.
+// null undefined false 0 은 무시된다.
+function Button ({ children, size = 'medium' }) {
     return (
-        <button className="Button">{ children }</button>
+        <>
+            {/* Array.join을 활용한 방법 */}
+            {/* <button className={['Button', size].join(' ')}>{ children }</button> */}
+            {/* classNames 모듈을 활용한 방법 */}
+            <button className={classNames('Button', size)}>{ children }</button>
+        </>
+        
     );
+};
+
+
+// 가장 정석적인 방법은 defaultProps를 사용하는것이다.
+Button.defaultProps = {
+    size: 'medium',
 };
 
 export default Button;
