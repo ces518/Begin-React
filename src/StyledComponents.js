bbac7029745b2f850707e6511f16893a04766b38
ces518 <pupupee9@gmail.com>
2019-12-08 23:05:11 +0900

    42.Styled Components

---
+++ src/StyledComponents.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Circle = styled.div`
+    width: 5rem;
+    height: 5rem;
+    background: ${props => props.color};
+    border-radius: 50%;
+    ${props => props.huge && css` 
+        width: 10rem;
+        height: 10rem;
+    `}
+`;
+
+// ${props => props.huge && ``}; 의 형태로 사용하면 동작은 한다.
+// 단점은 해당 함수 내부에서의 템플릿 리터럴은 Tagged Template Literal이 아니기 때문에 props를 더 받아올 수 없다.
+// 이를 해결하기 위해 styled-components의 css를 가져와서 이를 사용해주면 props를 사용이 가능하다.
+
+function StyledComponents () {
+    return (
+        <>
+            <Circle color="blue" huge/>
+        </>
+    );
+};
+
+export default StyledComponents;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Scss from './Scss';
+import StyledComponents from './StyledComponents';
 
 // id가 root인것을 찾아 리액트 앱을 넣어준다.
 // ReactApp을 랜더링할때는 root 엘리먼트 (div) 안에 랜더링 하는것이다.
-ReactDOM.render(<Scss />, document.getElementById('root'));
+ReactDOM.render(<StyledComponents />, document.getElementById('root'));
 
 
 serviceWorker.unregister();
