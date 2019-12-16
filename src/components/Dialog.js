28e76eeb8e1290d3a14944e8339404e8610bdbe5
ces518 <pupupee9@gmail.com>
2019-12-16 22:10:30 +0900

    48.StyledComponents_트랜지션구현하기.md

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,6 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes, css } from 'styled-components';
 import NewButton from './NewButton';
+
+const fadeIn = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`;
+
+const fadeOut = keyframes`
+    from {
+        opacity: 1;
+    }
+    to {
+        opacity: 0;
+    }
+`;
+
+const slideUp = keyframes`
+    from {
+        transform: translateY(200px);
+    }
+    to {
+        transform: translateY(0px);
+    }
+`;
+
+const slideDown = keyframes`
+    from {
+        transform: translateY(0px);
+    }
+    to {
+        transform: translateY(200px);
+    }
+`;
 
 const DarkBackground = styled.div`
     position: fixed;
@@ -12,6 +48,16 @@ const DarkBackground = styled.div`
     align-items: center;
     justify-content: center;
     background: rgba(0, 0, 0, 0.8);
+
+    animation-duration: 0.25s;
+    /* 빨라졌다 천천히 느려짐 */
+    animation-timing-function: ease-out; 
+    animation-name: ${fadeIn};
+    animation-fill-mode: forwards;
+
+    ${props => props.disappear && css`
+        animation-name: ${fadeOut};
+    ` };
 `;
 
 const DialogBlock = styled.div`
@@ -29,6 +75,16 @@ const DialogBlock = styled.div`
     p {
         font-size: 1.125rem;
     }
+
+    animation-duration: 0.25s;
+    /* 빨라졌다 천천히 느려짐 */
+    animation-timing-function: ease-out; 
+    animation-name: ${slideUp};
+    animation-fill-mode: forwards;
+
+    ${props => props.disappear && css`
+        animation-name: ${slideDown};
+    ` };
 `;
 
 const ButtonGroup = styled.div`
@@ -45,10 +101,24 @@ const ShortMarginButton = styled(NewButton)`
 `;
 
 function Dialog ({ title, children, confirmText, cancelText, visable, onConfirm, onCancel }) {
-    if (!visable) return null;
+    const [animate, setAnimate] = useState(false);
+    const [localVisible, setLocalVisible] = useState(visable);
+
+    useEffect(() => {
+        // Dialog 가 켜져있다가 닫히는 시점을 캐치하기위해 localVisible 이라는 state값을 사용한다.
+        if (localVisible && !visable) {
+            setAnimate(true);
+            setTimeout(() => setAnimate(false), 250);
+        }
+        // visible 값이 바뀔때마다 localVisible 값에 동기화
+        setLocalVisible(visable);
+    }, [localVisible, visable]);
+
+    if (!localVisible && !animate) return null;
+
     return (
-        <DarkBackground>
-            <DialogBlock>
+        <DarkBackground disappear={!visable}>
+            <DialogBlock disappear={!visable}>
                 <h3>{title}</h3>
                 <p>{children}</p>
                 <ButtonGroup>
