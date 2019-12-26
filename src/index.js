14416ac47ea5cc151402cd5153ddbb441e135bf7
ces518 <pupupee9@gmail.com>
2019-12-26 22:50:57 +0900

    49.TodoList 프로젝트 생성 UI 준비 1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Scss from './Scss';
-import StyledComponents from './StyledComponents';
+import App from './todolist/App';
 
 // id가 root인것을 찾아 리액트 앱을 넣어준다.
 // ReactApp을 랜더링할때는 root 엘리먼트 (div) 안에 랜더링 하는것이다.
-ReactDOM.render(<StyledComponents />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 
 serviceWorker.unregister();
