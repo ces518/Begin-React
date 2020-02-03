82e393b6e38aaaa9f98a5c5391a558c7040e8314
ces518 <pupupee9@gmail.com>
2020-02-03 15:23:10 +0900

    57.react async로 요청상태 관리하기

--- src/api_starter/User.js
+++ src/api_starter/User.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+// import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 
-async function getUser (id) {
+async function getUser ({ id }) {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
     return response.data;
 };
 
 function User ({ id }) {
-    const [state] = useAsync(() => getUser(id), [id]);
-    const { loading, data: user, error} = state;
+    const {
+        data: user,
+        error,
+        isLoading,
+        // skip true 와 같은 기능을 쓰고싶다면 run 을 불러온다.
+        // run 
+    } = useAsync({
+        // 만약 skip true 와 같은 기능을 하고싶다면 
+        // defferFn 으로 getUser 를 넣어준다.
+        promiseFn: getUser,
+        id,
+        watch: id, // id값이 바뀌면 다시호출 한다.
+        // reload : refetch 와 동일한 기능을 하는 함수
+    });
 
-    if (loading) return <div>로딩중</div>;
+    if (isLoading) return <div>로딩중</div>;
     if (error) return <div>에러가 발생했습니다.</div>;
     if (!user) return null;
 
